b4b0ac062e42237108f8378889db2de0af45899e
Adilabbas23 <adiljattjatt243@gmail.com>
2024-10-21 09:59:11 +0500

    uploaded java doc

---
+++ suit.java
@@ -0,0 +1,14 @@
+package labexam.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class suit {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
